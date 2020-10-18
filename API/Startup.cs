2ce175b8c87a8179a6d63f990bf9860cf9fde3dd
onurkybsi <onurbpm@outlook.com>
2020-10-18 14:08:08 +0300

    Scoped -> Singleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -5,7 +5,6 @@ using DataAccess.Abstract.IPersonRepo;
 using DataAccess.Abstract.PersonRepo;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,8 +29,8 @@ namespace API
             services.AddSingleton<IPersonRepositorySettings>(sp =>
                 sp.GetRequiredService<IOptions<PersonRepositorySettings>>().Value);
 
-            services.AddScoped<IPersonRepository, PersonRepository>();
-            services.AddScoped<IPersonService, PersonService>();
+            services.AddSingleton<IPersonRepository, PersonRepository>();
+            services.AddSingleton<IPersonService, PersonService>();
 
 
             services.AddMvc();
